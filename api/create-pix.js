2367cb1b1c3049467372d437929ce486b238c92c
ronald-almeida <114614394+ronald-almeida@users.noreply.github.com>
2026-09-04 05:06:19 -0300

    Update product amount in create-pix.js

--- api/create-pix.js
+++ api/create-pix.js
@@ -3,7 +3,7 @@ const API_BASE = 'https://api.gatewaypayshark.com.br';
 const PRODUCT = Object.freeze({
   name: 'MENTORIA TRT NO ALVO',
   description: 'MENTORIA TRT NO ALVO - Renata Fayad',
-  amount: 39770,
+  amount: 9770,
   currency: 'BRL',
   type: 'DIGITAL'
 });
